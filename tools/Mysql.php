27ed28f0e52ee81f6a87d34132132e6f7c6a996e
Ryan Knuesel <chawezul@gmail.com>
2012-07-22 13:04:32 -0500

    [#23] adding placeholder file

---
+++ tools/Mysql.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Zule\Tools;
+
+class Mysql
+{
+    // Welcome all my people to the greatest wonder of the world
+    // At this time, I am not supporting MySQL. It's a huge pain
+    // in my rear end. One day though, we will, either I'll write
+    // it, or someone else will. 
+    // This is [#23].
+}
+
